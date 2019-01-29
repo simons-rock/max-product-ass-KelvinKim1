30f44436dd8335392e7f10cb9583d53902e8a36f
김예웅 <macbook@maegbug-ui-MacBook-Pro.local>
2019-01-29 15:58:32 -0500

    finished

--- max_product_fast.c
+++ max_product_fast.c
@@ -1,6 +1,28 @@
 #include "max_product.h"
 
+void swap(int a, int b, int*arr) {
+    int temp;
+    temp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = temp;
+}
+
+
 int max_pairwise_product_fast(int *arr, int size){
-	return 0;
+    int index = 0;
+    for (int i = 1; i < size; i++){
+        if (arr[index] < arr[i]){
+            index = i;
+        }
+    }
+    swap (index, size-1, arr);
+    index = 0;
+    for (int i = 1; i < size-1; i++){
+        if (arr[index] < arr[i]){
+            index = i;
+        }
+    }
+    swap (index, size-2, arr);
+    return arr[size-1]*arr[size-2];
 }
 
